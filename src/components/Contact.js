0dd01d0716eba573fa9a86a9363c16093798651b
chukwutem-emi <chukwutememi@gmail.com>
2024-09-17 14:46:08 +0100

    updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,14 @@
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact us page</h1>
+        <div className="w-[40rem] my-0 mx-auto">
+            <form className="w-[30rem] box-border p-4 shadow-2xl mt-[4rem] my-0 mx-auto">
+                <h1 className="font-bold text-3xl box-border text-center">Contact us page</h1>
+                <label className="block text-[1.2rem]">Name:</label>
+                <input type="text" className="outline-none border-black border-solid border-[1px] text-center font-sans p-2 mb-4 rounded-lg box-border w-[100%]" placeholder="name"/>
+                <label className="block text-[1.2rem]">Message:</label>
+                <input type="text" className="outline-none border-black border-solid border-[1px] text-center font-sans p-2 box-border w-[100%] mb-4 rounded-lg" placeholder="message"/>
+                <button type="submit" className="p-2  text-white bg-green-900 cursor-pointer w-[100%] box-border rounded-lg">Submit</button>
+            </form>
         </div>
     );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
   // if dependency array is not empty === [i.e if there is value here] => useEffect is called every time the dependency changes.
     return (
       <div className="flex justify-between bg-pink-300 shadow-lg sm:bg-green-400 lg:bg-yellow-400 sm:min-w-64 lg:min-w-[900px] top-0 right-0 left-0 z-40 bg-fixed
-      ">
+      pb-8">
         <div className="header">
           <div className="logo-container">
             <img
@@ -23,17 +23,17 @@ const Header = () => {
             />
           </div>
           <div className="nav-items">
-            <ul className="flex ml-[20rem]">
+            <ul className="flex ml-[15rem]">
               <li className="mr-8">
                 Online Status: {onlineStatus ? "🟢":"🔴"}
               </li>
               <li className="mr-8">
                 <Link to="/">Home</Link>
               </li>
-              <li>
+              <li className="mr-8">
                 <Link to="/about">About us</Link>
               </li>
-              <li>
+              <li className="mr-8">
                 <Link to="/contact">Contact us</Link>
               </li>
               <li className="px-4">
